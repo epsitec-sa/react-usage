53f27d257974485aba966cfec62e4909df9d726d
Pierre Arnaud <arnaud@epsitec.ch>
2015-11-20 06:09:41 +0100

    Test how renderToStaticMarkup handles React.createElement output.

--- src/test/react.dom-server-usage.js
+++ src/test/react.dom-server-usage.js
@@ -17,10 +17,29 @@ describe ('ReactDOMServer', () => {
       expect (output).to.endWith ('</div>');
     });
   });
+
   describe ('renderToStaticMarkup()', () => {
     it ('produces HTML output', () => {
       const output = ReactDOMServer.renderToStaticMarkup (<Hello id='x' name='Joe'/>);
       expect (output).to.equal ('<div id="x">Hello Joe</div>');
+    });
+
+    it ('produces HTML output on React.createElement', () => {
+      const element = React.createElement ('img', {src: 'avatar.png', className: 'profile'});
+      const output  = ReactDOMServer.renderToStaticMarkup (element);
+      expect (output).to.equal ('<img src="avatar.png" class="profile"/>');
+    });
+
+    it ('produces HTML output on React.createElement, custom component', () => {
+      const element = React.createElement (Hello, {id: 'x', name: 'Joe'});
+      const output  = ReactDOMServer.renderToStaticMarkup (element);
+      expect (output).to.equal ('<div id="x">Hello Joe</div>');
+    });
+
+    it ('produces HTML output on React.createElement, custom component - unresolved', () => {
+      const element = React.createElement ('Hello', {id: 'x', name: 'Joe'});
+      const output  = ReactDOMServer.renderToStaticMarkup (element);
+      expect (output).to.equal ('<Hello id="x" name="Joe"></Hello>');
     });
   });
 });

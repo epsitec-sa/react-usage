043bc19b4c9769f81b777ab71d552fe3d29efdcf
Pierre Arnaud <arnaud@epsitec.ch>
2015-11-30 07:01:34 +0100

    Include missing changes.

--- src/test/arc.basic-usage.js
+++ src/test/arc.basic-usage.js
@@ -1,16 +1,17 @@
 'use strict';
 
-import {E} from '../themes/e.js';
-import {all} from 'electrum-arc/lib/all-components.js';
+import E from '../themes/e.js';
 
-console.log ('E initialized: ' + E);
+console.log ('E initialized:', E.typo);
 
 import Arc from 'electrum-arc';
+import {Button} from 'electrum-arc';
 
 describe ('Arc', () => {
   describe ('import', () => {
     it ('gives access to all components', () => {
       console.log (Arc);
+      console.log (E.components.Button === Button);
     });
   });
 });

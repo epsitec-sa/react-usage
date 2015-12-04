e412f760ffd08ce236aa0e538dbd5b6d9c3d249d
Pierre Arnaud <arnaud@epsitec.ch>
2015-12-04 04:57:58 +0100

    Experiment with new Babel legacy decorator.

--- src/test/babel-6-decorators.js
+++ src/test/babel-6-decorators.js
@@ -1,20 +1,48 @@
 'use strict';
 
-var fooCalled = 0;
+import {expect} from 'mai-chai';
+
+let fooCalled = 0;
+let fooClassBefore;
+let fooClassAfter;
 
 function foo (x) {
   fooCalled++;
-  return x;
+  class Bar extends x {
+    bar () {
+      return 'bar';
+    }
+  };
+  fooClassBefore = x;
+  fooClassAfter = Bar;
+  return Bar;
 }
 
-// @foo
+@foo
 class Foo {
+  bar () {
+    return 'foo';
+  }
 }
 
 describe ('Babel 6 and ES7 decorators', () => {
   describe ('@foo class Foo', () => {
     it ('invokes decorator', () => {
-      // expect (fooCalled).to.equal (1);
+      expect (fooCalled).to.equal (1);
+    });
+
+    it ('produces a new class', () => {
+      const foo = new Foo ();
+      expect (Foo).to.equal (fooClassAfter);
+      expect (Foo).to.not.equal (fooClassBefore);
+      expect (foo).to.have.property ('bar');
+      expect (foo.bar ()).to.equal ('bar');
+    });
+
+    it ('does not alter original class', () => {
+      const foo = new fooClassBefore (); // jscs:disable
+      expect (foo).to.have.property ('bar');
+      expect (foo.bar ()).to.equal ('foo');
     });
   });
 });
